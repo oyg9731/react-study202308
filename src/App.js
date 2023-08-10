9a1a0408777cd2f13171cddc6fe6bc153d896b5b
oh young seok <oyg9731@naver.com>
2023-08-10 14:30:38 +0900

    컴포넌트 기초

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import './App.css';
+import AppItem from './AppItem';
+import Noname from './Noname';
+import Hello from './Hello';
 
-function App() {
+const App = () => {
   // const $h2 = React.createElement('h2', null, '반가워요~~~')
   const $h2 = <h2>반가워요</h2>;
   return (
     <>
-      <div className="App">
-        <h1>빵빵뿌슝</h1>
-        {$h2}
-      </div>
-      <div className="noname">
-        <p>
-          <input type='text' /><br/>
-          오늘은 태풍이 와서 비가 많이 내렸다.<br/>
-          그래서 공부가 싫다.
-        </p>
-      </div>
+    <AppItem />
+    <Hello />
+    <Noname />
     </>
   );
 }
