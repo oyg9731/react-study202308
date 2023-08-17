5702e1c1684bc6298b971e5f7f615e2c8aba399e
oh young seok <oyg9731@naver.com>
2023-08-17 11:38:52 +0900

    목표설정 삭제 및 수정, 동적 클래스 변경

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CourseInput.css';
-import Button from '../UI/Button/Button';
+import Button from '../UI/Button/Button.js';
 
-const CourseInput = () => {
+const CourseInput = ({ onAdd }) => {
+
+  const [enteredText, setEnteredText] = useState('');
+  // 입력값 검증 상태변수
+  const [isValid, setIsValid] = useState(true);
+
+  const textChangeHandler = e => {
+    if (e.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+
+    // console.log(e.target.value);
+    setEnteredText(e.target.value);
+  };
+
+  const formSubmitHandler = e => {
+    e.preventDefault();
+    // console.log(enteredText);
+
+    if (enteredText.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+
+    onAdd(enteredText);
+    setEnteredText('');
+  };
+
   return (
-    <form>
-      <div className="form-control">
+    <form onSubmit={formSubmitHandler}>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>나의 목표</label>
-        <input type="text"/>
+        <input
+          type="text"
+          onChange={textChangeHandler}
+          value={enteredText} />
       </div>
       <Button type="submit">목표 추가하기</Button>
     </form>
-  )
-}
+  );
+};
 
-export default CourseInput
+export default CourseInput;
--- src/components/CourseGoals/CourseList.js
+++ src/components/CourseGoals/CourseList.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import './CourseList.css'
+import React from 'react';
+import './CourseList.css';
+import CourseItem from './CourseItem';
 
-const CourseList = () => {
+const CourseList = ({ items, onDelete }) => {
   return (
-    <ul className='goal-list'></ul>
-  )
-}
+    <ul className='goal-list'>
+      {items.map((item) => {
+        return <CourseItem key={item.id} item={item} onDelete={onDelete} />;
+      })}
+    </ul>
+  );
+};
 
-export default CourseList
+export default CourseList;
